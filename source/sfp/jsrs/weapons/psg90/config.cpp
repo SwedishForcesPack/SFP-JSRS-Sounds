f843f5571f585d1410bc65e3d511d090c111d11c
Björn Dahlgren <bjorn@dahlgren.at>
2017-11-19 15:21:46 +0100

    Support for Psg90

---
+++ source/sfp/jsrs/weapons/psg90/config.cpp
@@ -0,0 +1,36 @@
+class CfgPatches {
+    class sfp_jsrs_weapons_psg90 {
+        name = "sfp_jsrs_weapons_psg90";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"sfp_psg90", "JSRS_SOUNDMOD_Weapons"};
+        addonRootClass = "sfp_psg90";
+        author = "Swedish Forces Pack";
+        url = "https://www.anrop.se";
+    };
+};
+
+class Mode_SemiAuto;
+class Mode_FullAuto;
+
+class CfgWeapons 
+{
+    class Rifle_Base_F;
+    
+    class sfp_psg90_base : Rifle_Base_F
+    {
+        class Single: Mode_SemiAuto
+        {
+            class BaseSoundModeType;
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"jsrs_mx2010_shot_soundset","jsrs_mx2010_shell_soundset","jsrs_12x7mm_reflector_1"};
+            };
+            class SilencedSound
+            {
+                soundSetShot[] = {"jsrs_mx2010_shot_silenced_soundset","jsrs_mx2010_shell_soundset","jsrs_9x3mm_SD_reflector_1"};
+            };
+        };
+    };
+};
